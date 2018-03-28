563f455669a3cbb4b6da247557dcf4cd9cf8b544
Christof Stocker <stocker.christof@gmail.com>
2018-03-28 23:17:14 +0200

    allow appending to existing logfile

--- src/logfile.jl
+++ src/logfile.jl
@@ -12,21 +12,24 @@ struct Logfile
     name::String
     path::String
 
-    function Logfile(name::String, path::String = pwd())
-        path = endswith(path, "/") || isdir(path) ? joinpath(path, _make_filename(path, name)) : path
+    function Logfile(name::String, path::String = pwd(); append = false)
+        path = endswith(path, "/") || isdir(path) ? joinpath(path, _make_filename(path, name, append)) : path
         path = endswith(path, ".h5") ? path : path * ".h5"
         mkpath(dirname(path))
-        h5open(path, "w") do root
-            attrs(root)[ATTR_NETWORKNAME] = name
+        if !append || !isfile(path)
+            h5open(path, "w") do root
+                attrs(root)[ATTR_NETWORKNAME] = name
+            end
         end
         new(name, path)
     end
 end
 
-function _make_filename(dir, name)
+function _make_filename(dir, name, append = false)
     basename = replace(lowercase(name), r"\W", "")
     fname = basename
     i = 1
+    append && return fname
     while isfile(joinpath(dir, fname * ".h5"))
         fname = basename * "_" * string(i)
         i += 1
