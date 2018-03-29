58969c5fb5e070da72f92166f2fcc7e036e4455f
Christof Stocker <stocker.christof@gmail.com>
2018-03-29 17:06:40 +0200

    disconnect logfile name from network name

--- src/logfile.jl
+++ src/logfile.jl
@@ -1,32 +1,30 @@
 """
-    Logfile(name, [path = pwd()])
+    Logfile(id, [path = pwd()])
 
-Create a new logfile with the given network `name` at `path`.
+Create a new logfile with the given network `id` at `path`.
 
-Note that the `name` does not have to be equal (or similar for
-that matter) to the file name in `path`. Instead it denotes the
-internal name of the network (stored as an attribute in the
-file), which can be shared across log files.
+Note that the `id` does not have to be equal (or similar for
+that matter) to the file name in `path`.
 """
 struct Logfile
-    name::String
+    id::String
     path::String
 
-    function Logfile(name::String, path::String = pwd(); append = false)
-        path = endswith(path, "/") || isdir(path) ? joinpath(path, _make_filename(path, name, append)) : path
+    function Logfile(id::String, path::String = pwd(); append = false)
+        path = endswith(path, "/") || isdir(path) ? joinpath(path, _make_filename(path, id, append)) : path
         path = endswith(path, ".h5") ? path : path * ".h5"
         mkpath(dirname(path))
         if !append || !isfile(path)
             h5open(path, "w") do root
-                attrs(root)[ATTR_NETWORKNAME] = name
+                # attrs(root)[ATTR_NETWORKNAME] = id
             end
         end
-        new(name, path)
+        new(id, path)
     end
 end
 
-function _make_filename(dir, name, append = false)
-    basename = replace(lowercase(name), r"\W", "")
+function _make_filename(dir, id, append = false)
+    basename = replace(id, r"\W", "")
     fname = basename
     i = 1
     append && return fname
